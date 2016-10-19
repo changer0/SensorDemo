4c09d39fb963a6cf9fd3e0f8c30c01bc47379c1f
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-10-19 12:27:08 +0800

    重力传感器的使用

--- app/src/main/java/com/lulu/sensordemo/MainActivity.java
+++ app/src/main/java/com/lulu/sensordemo/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mManager;
     private Sensor mLightSensor;
     private TextView mTxtInfo;
+    private Sensor mGravitySensor;
+    private Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +51,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 mManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_UI);
             }
 
+            // ----------------------------
+            // 设置重力传感器
+            mGravitySensor = mManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+            if (mGravitySensor != null) {
+                mManager.registerListener(this, mGravitySensor, SensorManager.SENSOR_DELAY_UI);
+            }
+
+            // ----------------------------
+            //设置加速度传感器
+            mAccelerometer = mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (mAccelerometer != null) {
+                mManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+            }
+
+
         }
 
 
@@ -57,12 +75,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onDestroy() {
         // !!! 一定要取消注册, 否则会造成Activity内存泄漏
         if (mManager != null) {
-
-            if (mLightSensor != null) {
-                //取消特定传感器的监听, 如果监听器还有其他传感器, 那么仍然可以使用不会被取消
-                mManager.unregisterListener(this, mLightSensor);
-            }
-            //mManager.unregisterListener(this);//取消监听器所处理的所有 传感器的检测
+            //以下代码可以终止单个传感器的检测
+//            if (mLightSensor != null) {
+//                //取消特定传感器的监听, 如果监听器还有其他传感器, 那么仍然可以使用不会被取消
+//                mManager.unregisterListener(this, mLightSensor);
+//            }
+            mManager.unregisterListener(this);//取消监听器所处理的所有 传感器的检测
         }
         super.onDestroy();
     }
@@ -83,39 +101,87 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // 1. 处理传感器数据, 先检查传感器的类型;
         int type = event.sensor.getType();
         if (type == Sensor.TYPE_LIGHT) {
-            // 2. 处理传感器的数值
-            float[] values = event.values;
-            // 3. 亮度数值 只有一个元素, 根据 SensorManger常量来处理
-            float light = values[0];
-            mTxtInfo.setText(String.valueOf(light));
-
-            float screenLight = 0;
-            if (light >= SensorManager.LIGHT_NO_MOON && light < SensorManager.LIGHT_FULLMOON) {
-                screenLight = 0.1f;
-            }else if (light >= SensorManager.LIGHT_FULLMOON && light < SensorManager.LIGHT_CLOUDY) {
-                screenLight = 0.2f;
-            }else if (light >= SensorManager.LIGHT_CLOUDY && light < SensorManager.LIGHT_SUNRISE) {
-                screenLight = 0.3f;
-            }else if (light >= SensorManager.LIGHT_SUNRISE && light < SensorManager.LIGHT_OVERCAST) {
-                screenLight = 0.4f;
-            }else if (light >= SensorManager.LIGHT_OVERCAST && light < SensorManager.LIGHT_SHADE) {
-                screenLight = 0.5f;
-            }else if (light >= SensorManager.LIGHT_SHADE && light < SensorManager.LIGHT_SUNLIGHT) {
-                screenLight = 0.6f;
-            }else if (light >= SensorManager.LIGHT_SUNLIGHT && light < SensorManager.LIGHT_SUNLIGHT_MAX) {
-                screenLight = 0.7f;
-            }else if (light >= SensorManager.LIGHT_SUNLIGHT_MAX ) {
-                screenLight = 1f;
-            }
-
-            // TODO: 2016/10/19 调整屏幕亮度 (针对Activity)
-
-            adjustScreenLight(screenLight);
+            //processLight(event);
+        } else if (type == Sensor.TYPE_GRAVITY) {
+            //processGravity(event);
+        } else if (type == Sensor.TYPE_ACCELEROMETER) {
+            processAccelerometer(event);
         }
     }
+
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+
+    /**
+     * 处理加速度
+     * @param event
+     */
+    private int mShakeCount = 0;
+    private void processAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+        StringBuilder sb = new StringBuilder();
+        sb.append("AX: ").append(x).append('\n');
+        sb.append("AY: ").append(y).append('\n');
+        sb.append("AZ: ").append(z).append('\n');
+        mTxtInfo.setText(sb.toString());
+
+        float ax = Math.abs(x);
+        float ay = Math.abs(y);
+        float az = Math.abs(z);
+        //计算绝对值, 超过12代表大幅度的晃动
+
+        if (ax >= 12 || ay >= 12 || az >= 12) {
+            mShakeCount++;
+            if (mShakeCount > 4) {
+                // TODO: 2016/10/19 摇了
+                Toast.makeText(this, "小伙你摇得不轻啊", Toast.LENGTH_SHORT).show();
+                mShakeCount = 0;
+            }
+        }
+
+    }
+
+
+    /**
+     * 处理亮度变化
+     * @param event
+     */
+    private void processLight(SensorEvent event) {
+        // 2. 处理传感器的数值
+        float[] values = event.values;
+        // 3. 亮度数值 只有一个元素, 根据 SensorManger常量来处理
+        float light = values[0];
+        mTxtInfo.setText(String.valueOf(light));
+
+        float screenLight = 0;
+        if (light >= SensorManager.LIGHT_NO_MOON && light < SensorManager.LIGHT_FULLMOON) {
+            screenLight = 0.1f;
+        }else if (light >= SensorManager.LIGHT_FULLMOON && light < SensorManager.LIGHT_CLOUDY) {
+            screenLight = 0.2f;
+        }else if (light >= SensorManager.LIGHT_CLOUDY && light < SensorManager.LIGHT_SUNRISE) {
+            screenLight = 0.3f;
+        }else if (light >= SensorManager.LIGHT_SUNRISE && light < SensorManager.LIGHT_OVERCAST) {
+            screenLight = 0.4f;
+        }else if (light >= SensorManager.LIGHT_OVERCAST && light < SensorManager.LIGHT_SHADE) {
+            screenLight = 0.5f;
+        }else if (light >= SensorManager.LIGHT_SHADE && light < SensorManager.LIGHT_SUNLIGHT) {
+            screenLight = 0.6f;
+        }else if (light >= SensorManager.LIGHT_SUNLIGHT && light < SensorManager.LIGHT_SUNLIGHT_MAX) {
+            screenLight = 0.7f;
+        }else if (light >= SensorManager.LIGHT_SUNLIGHT_MAX ) {
+            screenLight = 1f;
+        }
+
+        // TODO: 2016/10/19 调整屏幕亮度 (针对Activity)
+
+        adjustScreenLight(screenLight);
     }
 
     private void adjustScreenLight(float l) {
@@ -125,5 +191,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         window.setAttributes(params);
     }
 
+    /**
+     * 处理重力变化
+     * @param event
+     */
+    private void processGravity(SensorEvent  event) {
+        float[] values = event.values;
+        float gx = values[0];
+        float gy = values[1];
+        float gz = values[2];
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("GX: ").append(gx).append('\n');
+        sb.append("GY: ").append(gy).append('\n');
+        sb.append("GZ: ").append(gz).append('\n');
+        mTxtInfo.setText(sb.toString());
+
+
+    }
 
 }
